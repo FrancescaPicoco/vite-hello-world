caca2441f6d65ffe103fc31a4436b9df7a888566
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-11-28 09:46:36 +0100

    non va

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppTitle from './components/AppTitle.vue'
+import vueimg from './components/vueimg.vue'
+
+export default {
+  components: {
+    AppTitle,
+    vueimg
+  }
+}
+</script>
+
+<template>
+  <div>
+    Benvenuto nella tua App in Vue.js
+  </div>
+  <AppTitle />
+  <vueimg />
+</template>
+
+<style scoped></style>
+
+<style scoped></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<template>
+  <h1>CIAO</h1>
+</template>
+
+<style scoped></style>
